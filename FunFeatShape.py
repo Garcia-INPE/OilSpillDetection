f48b0b38941c40525f8d611832ac620373e77f64
Garcia-INPE <garcia.inpe@gmail.com>
2024-10-22 16:11:12 -0300

    Shape features extraction

---
+++ FunFeatShape.py
@@ -0,0 +1,69 @@
+import os
+import cv2
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+
+CRS = 3857  # TODO: Check!   # 3857 (meters) | 4326 (degress, WGS84)
+
+
+def get_shape_features(pol_deg, plot_pol=False):
+    # polygon's geometry georrefeenced in meters
+    pol_m = pol_deg.to_crs(CRS)
+    bounds = {'LOC_POL_BBOX_X1_DEG': float(pol_deg.bounds.minx.iloc[0]),
+              'LOC_POL_BBOX_X2_DEG': float(pol_deg.bounds.maxx.iloc[0]),
+              'LOC_POL_BBOX_Y1_DEG': float(pol_deg.bounds.miny.iloc[0]),
+              'LOC_POL_BBOX_Y2_DEG': float(pol_deg.bounds.maxy.iloc[0]),
+              'LOC_POL_BBOX_X1_M': float(pol_m.bounds.minx.iloc[0]),
+              'LOC_POL_BBOX_X2_M': float(pol_m.bounds.maxx.iloc[0]),
+              'LOC_POL_BBOX_Y1_M': float(pol_m.bounds.miny.iloc[0]),
+              'LOC_POL_BBOX_Y2_M': float(pol_m.bounds.maxy.iloc[0])}
+    # 'AREA_KM','PERIM_KM','COMPLEX_MEAS','SPREAD','SHP_FACT','HU_MOM','CIRCUL','PER_AREA_RATIO'])
+    # m/1000 = km
+    centr_m_lat = pol_m.centroid.iloc[0].y/1000
+    # m/1000 = km
+    centr_m_lon = pol_m.centroid.iloc[0].x/1000
+    # m2/1000 = km2
+    area_km = float(pol_m.area.iloc[0]/1000)
+    # m/1000 = in km
+    perim_km = float(pol_m.length.iloc[0]/1000)
+    complexity = float((perim_km ** 2) / area_km)
+    minx, miny, maxx, maxy = pol_m.total_bounds             # bounding box
+    len_km = (maxx - minx)/1000
+    wid_km = (maxy - miny)/1000
+    spread_km = float(wid_km / len_km)
+    shp_fact_km = float(perim_km / (4 * np.sqrt(area_km)))  # TODO: verificar
+    circul_km = float((perim_km ** 2) / (4 * np.pi * area_km))
+    perim_area_ratio = float(perim_km / area_km)
+
+    shp_feat = {'LOC_CENTR_KM_LAT': centr_m_lat, 'LOC_CENTR_KM_LON': centr_m_lon,
+                'SHP_AREA_KM2': area_km, 'SHP_PERIM_KM': perim_km, 'SHP_COMPLEX_MEAS': complexity,
+                'SHP_SPREAD': spread_km, 'SHP_FACT': shp_fact_km, 'SHP_CIRCUL': circul_km,
+                'SHP_PER_AREA_RATIO': perim_area_ratio}
+    hu_mom = get_huMomentun(pol_m)
+
+    # Merges the dictionaries before returning
+    return (bounds | shp_feat | hu_mom)
+
+
+def get_huMomentun(pol_m):
+    fname = "pol_huMom.jpg"
+    pol_m.plot()
+    # plt.show()
+    plt.savefig(fname)
+    plt.close()
+
+    # Read image as grayscale image
+    im = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
+    moments = cv2.moments(im)           # Calculate Moments
+    huMoments = cv2.HuMoments(moments)  # Calculate Hu Moments
+    # Hu Moments have a large range. Some hu[i] are not comparable in magnitude,
+    # a log transform brings them in the same range
+    i = 0
+    for i in range(0, 7):
+        huMoments[i] = -1 * \
+            math.copysign(1.0, huMoments[i][0]) * \
+            math.log10(abs(huMoments[i][0]))
+    os.remove(fname)
+
+    return {f"SHP_HU_MOM{(i+1)}": float(x[0]) for i, x in enumerate(huMoments)}
--- GetSlicks.py
+++ GetSlicks.py
@@ -1,27 +1,28 @@
-from matplotlib import pyplot as plt
+from rasterio.windows import Window
+from rasterio.mask import mask as rioMask
+import json
+import rasterio.mask
 from os.path import expanduser
-import numpy as np
 import rasterio
 import geopandas as gpd
-import cv2
 import pandas as pd
-import math
 import glob
 import csv
 import os
 import re
 import importlib
+from matplotlib import pyplot as plt
+from rasterio import plot as rasterplot
+from shapely.geometry import MultiPolygon, Polygon
 
-import FunStat as FStat
-importlib.reload(FStat)
+import FunFeatShape as FeatShp
+importlib.reload(FeatShp)
 
 # A cross-platform way to get the home directory
 home = expanduser("~")
 
-# from rasterio import plot as rasterplot
-
 pd.set_option("display.max_columns", 1000)
-CRS = 3857  # TODO: Check!
+CRS = 3857  # TODO: Check!   # 3857 (meters) | 4326 (degress, WGS84)
 
 dir_img = "./img"
 os.makedirs(dir_img, exist_ok=True)
@@ -49,17 +50,19 @@ assert len(fnames_img8) == len(fnames_img16)
 # pol=pol_deg
 
 
-# Geométricas (Melissa): area (a), perim (p), complexity_measure (c),  spreading (s), shape_factor, hu_moment, circularity, ratio
+# Geométricas: area, perim, complexity_measure, spreading, shape_factor, hu_moment, circularity, ratio
 with open('stats.csv', 'w') as f1:
     writer = csv.writer(f1, delimiter=";", lineterminator='\n')
-    _ = writer.writerow(['FNAME', 'VET_ID', 'POLY_ID', 'CENTR_KM_LAT', 'CENTR_KM_LON', 'AREA_KM2', 'PERIM_KM', 'COMPLEX_MEAS', 'SPREAD', 'SHP_FACT',
-                         'HU_MOM1', 'HU_MOM2', 'HU_MOM3', 'HU_MOM4', 'HU_MOM5', 'HU_MOM6', 'HU_MOM7', 'CIRCUL', 'RATIO'])
+    # _ = writer.writerow(['IMG_NAME', 'ID_VECT', 'ID_POLYG', 'CENTR_KM_LAT', 'CENTR_KM_LON',
+    #                      'SHP_AREA_KM2', 'SHP_PERIM_KM', 'SHP_COMPLEX_MEAS', 'SHP_SPREAD', 'SHP_FACT',
+    #                      'SHP_HU_MOM1', 'SHP_HU_MOM2', 'SHP_HU_MOM3', 'SHP_HU_MOM4', 'SHP_HU_MOM5', 'SHP_HU_MOM6', 'SHP_HU_MOM7',
+    #                      'SHP_CIRCUL', 'SHP_PER_AREA_RATIO'])
 
     idx_img = 0
     fname_img = fnames_img16[idx_img]
     # Loop pelas imagens TIF encontradas no diretório
     for idx_img in range(len(fnames_img16)):
-        fname_img = fnames_img8[idx_img]
+        fname_img = fnames_img16[idx_img]
         # Basename (nome do arquivo somente, sem o path)
         bname = os.path.basename(fname_img)
         # Pega o ID da imagem do nome, para poder pegar seus vetores
@@ -77,11 +80,11 @@ with open('stats.csv', 'w') as f1:
         tiff_extent = [tiff.bounds[0], tiff.bounds[2],
                        tiff.bounds[1], tiff.bounds[3]]
 
-        # Plota uma imagem com todos os polígono da imagem e uma imagem para cada polígono da imagem
+        # Plota uma imagem com todos os polígonos da imagem e uma imagem para cada polígono da imagem
         # vets_img.plot();
         idx_vet = 0
         for idx_vet in range(len(vets_img)):
-            # Captura polígono (pode ser multipolígonomesmo sendo polígono único)
+            # Captura polígono (pode ser multipolígono mesmo sendo polígono único)
             multipol_df = vets_img.iloc[[idx_vet]]
             # Transforma cada multipolígono lista de polígonos
             pol_df = multipol_df.explode(index_parts=True)
@@ -89,48 +92,93 @@ with open('stats.csv', 'w') as f1:
             pol = pol_df.iloc[idx_pol]
             for idx_pol in range(len(pol_df)):
                 # row = [bname, str(idx_vet).rjust(3, '0'), str(idx_pol).rjust(3, '0')]
-                row = [bname, idx_vet, idx_pol]
+                row = {'IMG_NAME': bname,
+                       'ID_VECT': idx_vet, 'ID_POLYG': idx_pol}
+
                 print(row)
-                # CRS=z   # 3857 (meters) | 4336 (degress)
                 # polygon's geometry georrefeenced in degrees
                 pol_deg = pol_df.iloc[[idx_pol]]
-                # polygon's geometry georrefeenced in meters
-                pol_m = pol_df.iloc[[idx_pol]].to_crs(CRS)
-                # 'AREA_KM','PERIM_KM','COMPLEX_MEAS','SPREAD','SHP_FACT','HU_MOM','CIRCUL','RATIO'])
-                # meter/1000 = in km
-                centr_m_lat = pol_m.centroid.iloc[0].y/1000
-                # meter/1000 = in km
-                centr_m_lon = pol_m.centroid.iloc[0].x/1000
-                # meter/1000 = in km2
-                area_km = pol_m.area.iloc[0]/1000
-                # m/1000 = in km
-                perim_km = pol_m.length.iloc[0]/1000
-                complexity = (perim_km ** 2) / area_km
-                minx, miny, maxx, maxy = pol_m.total_bounds             # bounding box
-                len_km = (maxx - minx)/1000
-                wid_km = (maxy - miny)/1000
-                spread_km = (wid_km / len_km)
-                shp_fact_km = perim_km / \
-                    (4 * np.sqrt(area_km))         # TODO: verificar
-                hu_mom = FStat.get_huMomentun(pol_m)
-                circul_km = (perim_km ** 2) / (4 * np.pi * area_km)
-                perim_area_ratio = perim_km / area_km
-                _ = writer.writerow(row + [area_km, centr_m_lat, centr_m_lon, perim_km,
-                                    complexity, spread_km, shp_fact_km] + hu_mom + [circul_km, perim_area_ratio])
+                # importlib.reload(FeatShp)
+                shape_features = FeatShp.get_shape_features(pol_deg)
 
-            # pol.geometry.plot(ax=ax, facecolor='w', edgecolor='red')
-            # pol.plot();
-            # plt.savefig(f"{dir_img}{os.sep}{bname.replace(".tif", "")}_Vet{str(idx_vet).rjust(3, '0')}_Pol{str(idx_pol).rjust(3, '0')}.png");
-            # plt.close();
-            # plt.show()
+                line = row | shape_features  # Contatena os dicts
+                # Escreve o header do CSV se for o primeiro polog analisado
+                if idx_img == 0 and idx_vet == 0 and idx_pol == 0:
+                    _ = writer.writerow(line.keys())
+                # Escreve a linha
+                _ = writer.writerow(line.values())
 
 
-# f, ax = plt.subplots()
+# Plotting
+pol_deg.geometry.plot(facecolor='w', edgecolor='red')
+# pol_deg.plot()
+# plt.savefig(f"{dir_img}{os.sep}{bname.replace(".tif", "")}_Vet{str(idx_vet).rjust(3, '0')}_Pol{str(idx_pol).rjust(3, '0')}.png");
+# plt.close();
+plt.show()
+
+print(pol_deg.bounds)
+f, ax = plt.subplots()
 # rasterplot.show(tiff,  # use tiff.read(1) with your data
-#     extent=tiff_extent,
-#     ax=ax,
-# )
-# # plot shapefiles
-# row.geometry.plot(ax=ax, facecolor='w', edgecolor='red')
-# #plt.savefig('test.jpg')
-# plt.show()
+#                 # minx, maxx, miny, maxy
+#                 extent=[pol_deg.bounds.minx, pol_deg.bounds.maxx,
+#                         pol_deg.bounds.miny, pol_deg.bounds.maxy],
+#                 ax=ax,
+#                 )
+
+data, _ = rasterio.mask.mask(tiff, shapes=seg, crop=True)
+rasterplot.show(data, ax=ax)
+# rasterplot.show(pol_deg, ax=ax)
+# plot shapefiles
+# pol_deg.geometry.plot(ax=ax, facecolor='w', edgecolor='red')
+# plt.savefig('test.jpg')
+plt.show()
+
+with rasterio.open(fname_img) as src:
+    data, _ = rasterio.mask.mask(src, shapes=bbox, crop=True)
+
+
+fig = plt.figure(figsize=[12, 8])
+# Plot the raster data using matplotlib
+ax = fig.add_axes([0, 0, 1, 1])
+raster_image = ax.imshow(data[0, :, :], cmap="terrain", vmax=5000, vmin=-4000)
+fig.colorbar(raster_image, ax=ax, label="Elevation (in m) ",
+             orientation='vertical', extend='both', shrink=0.5)
+plt.show()
+
+
+# ================================================================
+# Open the raster data using rasterio
+data, _ = rasterio.mask.mask(tiff, shapes=pol_deg.geometry, crop=True)
+
+fig = plt.figure(figsize=[12, 8])
+# Plot the raster data using matplotlib
+ax = fig.add_axes([0, 0, 1, 1])
+raster_image = ax.imshow(data[0, :, :], cmap="terrain", vmax=5000, vmin=-3000)
+fig.colorbar(raster_image, ax=ax, label="Elevation (in m) ",
+             orientation='vertical', extend='both', shrink=0.5)
+plt.show()
+
+
+xmin, xmax, ymin, ymax = float(pol_deg.bounds.minx), float(
+    pol_deg.bounds.maxx), float(pol_deg.bounds.miny), float(pol_deg.bounds.maxy)
+bbox = MultiPolygon(
+    [Polygon([[xmin, ymin], [xmin, ymax], [xmax, ymax], [xmax, ymin]])])
+tiff2 = rasterio.open(fname_img, masked=False, chunks=True)
+tiff2.shape
+out_img, out_transform = rioMask(tiff2, shapes=bbox, crop=False)
+ax = rasterplot.show(out_img, cmap="gray", shapes=bbox)
+# pol_deg.boundary.plot(ax=ax) # ax=ax)
+plt.show()
+
+
+# Check your coordinate ordering,
+# I wasn't sure which was X and which was Y
+ulx, uly = 5773695.0, 601200.0
+width, height = 700, 500
+
+with rasterio.open('/path/to/someraster') as ds:
+    # Note - index takes coordinates in X,Y order
+    #        and returns row, col (Y,X order)
+    row, col = ds.index(ulx, uly)
+    window = Window(row, col, width, height)
+    data = ds.read(window)
